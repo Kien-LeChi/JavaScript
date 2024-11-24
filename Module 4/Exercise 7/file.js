62977aa4cb35cd3a578e054d1e673d8af1897f4d
acidentally <chikienl@metropolia.fi>
2024-11-24 21:33:22 +0200

    Updated for better searching with spaces in address

--- Module 4/Exercise 7/file.js
+++ Module 4/Exercise 7/file.js
@@ -1,14 +1,16 @@
 'use strict';
 
 
+const api_key = 'a1e437f79628464c9ea8d542db6f6e94'; //Funny free api
+
 const map = L.map('map').setView([60.1785553, 24.8786212], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-let previous_line = [];
+let previous_line = [];  
 
 const api_routing = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
-const api_address_lookup = 'https://api.digitransit.fi/geocoding/v1/search';
+const api_address_lookup = 'https://api.digitransit.fi/geocoding/v1/search';    
 
 const form = document.querySelector("#form");
 
@@ -52,7 +54,7 @@ function get_route(origin, des) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'digitransit-subscription-key': 'da3998d39d6744a98a0604a4e6b5c933',
+            'digitransit-subscription-key': api_key,
         },
         body: JSON.stringify({ query: GQLQuery }),
     }
@@ -61,7 +63,9 @@ function get_route(origin, des) {
         return response.json();
     }).then(function (result) {
 
-        for (let i = previous_line.length - 1; i >= 0; i--)  {
+        // Remove previous search on the map or else there'll be
+        // multiple paths
+        for (let i = previous_line.length - 1; i >= 0; i--)  { 
             previous_line[i].removeFrom(map);
             previous_line.pop();
         }
@@ -69,11 +73,14 @@ function get_route(origin, des) {
         const cur_route = result.data.plan.itineraries[0].legs;
         let start_time = cur_route[0].startTime;
         let end_time = cur_route[0].endTime;
-        let total_distance = 0
+        let total_distance = 0;
         for (let i = 0; i < cur_route.length; i++) {
             end_time = cur_route[i].endTime;    
             total_distance += cur_route[i].distance;
             let color = '';
+
+            //Respectfully, stolen this color-coded modes from the given Examples 
+
             switch (cur_route[i].mode) {
                 case 'WALK':
                     color = 'green';
@@ -138,19 +145,21 @@ function get_route(origin, des) {
 form.addEventListener('submit', async (evt) => {
     evt.preventDefault();
 
-    const address = document.querySelector("#target").value;
-    console.log(address);
+    let address = document.querySelector("#target").value;
+    address = encodeURIComponent(address);
 
-    const search = api_address_lookup + `?text=${address}&digitransit-subscription-key=da3998d39d6744a98a0604a4e6b5c933`;
+    const search = api_address_lookup + `?text=${address}&digitransit-subscription-key=${api_key}`;
 
     let add_coords = await fetch(search).then(function (response) {
         return response.json();
     })
+    
     add_coords = add_coords.features[0].geometry.coordinates;
     console.log(add_coords[0], add_coords[1]);
 
     let start_lat = add_coords[1];
     let start_long = add_coords[0];
+
     //The latter coordinates is the school's address.
     get_route({ latitude: start_lat, longitude: start_long}, { latitude: 60.2241122, longitude: 24.7565199 });
 })
